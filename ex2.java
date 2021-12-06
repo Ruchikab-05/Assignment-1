e67fd47439091275797d6de649e54096658bafe3
Ruchikab-05 <95629253+Ruchikab-05@users.noreply.github.com>
2021-12-06 19:21:13 +0530

    Add files via upload

---
+++ ex2.java
@@ -0,0 +1,10 @@
+public class ex2{ 
+    public static void main(String[] args){
+        int a = 2;// declaring variable a
+        int b = 5;// declaring variable b
+        System.out.println("Enter two num:"+ a +" , " +b); //printing enter two num : 2 , 5
+
+        int sum =  a + b; // adding both the values
+        System.out.println("sum is:" + sum);//printing sum is: 7
+    }
+}
