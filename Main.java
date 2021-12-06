d7605138eeb879ce63ffc1a850166586dd797371
Ruchikab-05 <95629253+Ruchikab-05@users.noreply.github.com>
2021-12-06 19:15:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main{ 
+    public static void main(String[] args){
+        int a = -2;// declaring variable a
+        int b = -3;// declaring variable b
+        System.out.println("Enter two num:"+ a +" , " +b); //printing enter two num : -2 , -3
+
+        int sum =  a + b; // adding both the values
+        System.out.println("sum is:" + sum);//printing sum is: -5
+    }
+}
